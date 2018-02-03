048a888ad0c6dc53dfb389bfe4317d831f61ef91
koolii <koolii0909@gmail.com>
2018-02-04 00:26:23 +0100

    :+1: add comments in http-server-version-1.go

--- src/06/http-server-version-1.go
+++ src/06/http-server-version-1.go
@@ -15,9 +15,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Server is running at localhost:8080")
+	fmt.Println("[logger] Server is running at localhost:8080")
 
+	// この無限ループでリクエストを送信した時に、どういう挙動になっている？
 	for {
+		fmt.Println("[logger] Start New Loop and wait until a request come.")
 		conn, err := listener.Accept()
 		if err != nil {
 			panic(err)
@@ -26,12 +28,13 @@ func main() {
 		go func() {
 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
 
-			// 読み込み
+			// HTTPリクエストを分解
 			request, err := http.ReadRequest(bufio.NewReader(conn))
 			if err != nil {
 				panic(err)
 			}
 
+			// デバッグ用の関数で、分解したHTTPリクエストを出力してくれる
 			dump, err := httputil.DumpRequest(request, true)
 			if err != nil {
 				panic(err)
@@ -39,14 +42,18 @@ func main() {
 
 			fmt.Println(string(dump))
 
+			// HTTPレスポンスを生成
 			response := http.Response{
 				StatusCode: 200,
 				ProtoMajor: 1,
 				ProtoMinor: 0,
 				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
 			}
+			// HTTPレスポンスを返す
 			response.Write(conn)
 			conn.Close()
 		}()
+
+		fmt.Println("[logger] Finish this loop, but it hasn't handle response to a client yet")
 	}
 }
