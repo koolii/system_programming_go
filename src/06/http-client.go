c74cb851f0054e9d41fa357295156a8a68d2e942
koolii <koolii0909@gmail.com>
2018-02-04 01:56:36 +0100

    :+1: implement Keep-Alive into http client/server

--- src/06/http-client.go
+++ src/06/http-client.go
@@ -6,33 +6,71 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		panic(err)
+	// 今回は簡単化去せるために、予め送信メッセージを配列にためておいて、自動で完了するスクリプトにしている
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
+	}
+	current := 0
+	// var conn net.Conn = nil
+	conn, _ := net.Dial("tcp", "localhost:8080")
+
+	// リトライ用にループで全体を囲う
+	for {
+		var err error
+
+		if conn == nil {
+			// Dialから行ってconnを初期化
+			// ここでエラーが出てしまう src/06/http-client.go:xx:xx: conn declared and not used
+			// conn, err := net.Dial("tcp", "localhost:8080")
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("Access %d\n", current)
+		}
+
+		// POSTで文字列を送るリクエストを作成
+		fmt.Println("[logger] Make a request to server")
+		request, err := http.NewRequest("POST", "http://localhost:8080", strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("[logger] Send a request to server")
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("[logger] Get response from server")
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			// タイムアウトはここでエラーになるのでリトライする
+			fmt.Println("[logger] Retry")
+			conn = nil
+			continue
+		}
+
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("[logger] Output response information")
+		fmt.Println(string(dump))
+
+		// 全部送信完了していれば終了
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 	}
 
-	request, err := http.NewRequest("GET", "http://localhost:8080", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("[logger] send request to server")
-	request.Write(conn)
-
-	fmt.Println("[logger] get response from server")
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("[logger] output response information")
-	fmt.Println(string(dump))
+	conn.Close()
 }
--- src/06/http-server.go
+++ src/06/http-server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -19,41 +21,72 @@ func main() {
 
 	// この無限ループでリクエストを送信した時に、どういう挙動になっている？
 	for {
-		fmt.Println("[logger] Start New Loop and wait until a request come.")
+		// コネクションを生成する時はここからスタートする
+		fmt.Println("[logger] Create a new TCP connection and wait until a request come.")
 		conn, err := listener.Accept()
 		if err != nil {
 			panic(err)
 		}
 
 		go func() {
+			defer conn.Close()
 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
 
-			// HTTPリクエストを分解
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
-			}
+			// Accept後のソケットで何度も応答を返す為にループ
+			// ここでループを追加することによって、TCPコネクションが張られた後も何度もリクエストを処理出来るようになる
+			// => 無限ループ
+			for {
+				fmt.Println("[logger] From now on, it sets timeout into conn")
+				// タイムアウトを設定(無限ループの終了条件)
+				// ここで設定しておくことで、通信がしばらくないとタイムアウトのエラーでRead()の呼び出しを終了する
+				// タイムアウトを設定しないとコネクションがブロックされたままになってしまう
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				// HTTPリクエストを分解
+				// net.Connをbufio.Readerでラップして、それをhttp.ReadRequest()関数に渡している
+				request, err := http.ReadRequest(bufio.NewReader(conn))
 
-			// デバッグ用の関数で、分解したHTTPリクエストを出力してくれる
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
+				if err != nil {
+					// 標準のerrorインタフェースの上位互換であるnet.Errorインタフェースの構造体から取得可能
+					// タイムアウト時のエラーはnet.Connが作成するが、それ以外のio.Readerは最初に発生したエラーをそのまま伝搬する
+					// だからerrorからダウンキャストを行うことでタイムアウトかどうかを判断出来る
+					neterr, ok := err.(net.Error)
+					if ok && neterr.Timeout() {
+						// タイムアウトもしくはソケットクローズ時は終了
+						fmt.Println("[logger] Timeout")
+						break
+					} else if err == io.EOF {
+						fmt.Println("[logger] EOF")
+						break
+					}
+					// それ以外はエラーにする
+					panic(err)
+				}
 
-			fmt.Println(string(dump))
+				// デバッグ用の関数で、分解したHTTPリクエストを出力してくれる
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
 
-			// HTTPレスポンスを生成
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
+				fmt.Println(string(dump))
+
+				// HTTPレスポンスを生成
+				// ContentLengthを返す為にcontent変数に返却する文字列で初期化する
+				// Go言語だと、HTTP/1.0以下だったり、ContentLengthが無いとConnection: closeヘッダを付与する仕様がある
+				content := "Hello World\n"
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
+					ContentLength: int64(len(content)),
+					Body:          ioutil.NopCloser(strings.NewReader(content)),
+				}
+				// HTTPレスポンスを返す
+				response.Write(conn)
 			}
-			// HTTPレスポンスを返す
-			response.Write(conn)
-			conn.Close()
 		}()
 
+		// コネクションがタイムアウトすると、次のリクエストからは下記のログからスタートする
 		fmt.Println("[logger] Finish this loop, but it hasn't handle response to a client yet")
 	}
 }
